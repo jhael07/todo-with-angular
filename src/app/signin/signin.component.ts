9102f2bc7e6945430c5028ee59c5fdc728333d80
Djoelvl <91484448+djoelvlFMP@users.noreply.github.com>
2023-05-23 10:16:21 -0400

    login responsive

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.css']
+  styleUrls: ['./signin.component.css'],
 })
 export class SigninComponent {
+  username = '';
+  password = '';
 
+  // * 👇 This function validate if the login credentials are fill out
+  validation() {
+    if (!this.username || !this.password) {
+      Swal.fire('Error', 'The username or password is invalid', 'error');
+      return;
+    }
+    // * 👇 If there are no errors, it triggers this function
+    this.login();
+  }
+
+  login() {
+    localStorage.setItem(
+      'session',
+      JSON.stringify({ username: this.username })
+    );
+
+    Swal.fire(
+      ` ${this.username}, you have loggin sucessfully 😀!!`,
+      'Now you are able to save your tasks in the browser.',
+      'success'
+    );
+
+    setTimeout(() => {
+      window.location.href = '/';
+    }, 4000);
+  }
 }
